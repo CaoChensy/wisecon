66481e69912e4c1882e59d70a89c23d6935551ac
chensy <chensy.cao@foxmail.com>
2024-10-06 09:52:36 +0800

    20241005: add utils.

---
+++ quant_tools/utils/request.py
@@ -0,0 +1,8 @@
+__all__ = [
+    "headers",
+]
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+}
