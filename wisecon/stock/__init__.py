89b7e5d6023d55908e7306f718c4f74b28a74ea8
CaoChensy <867529463@qq.com>
2024-10-24 09:09:11 +0800

    20241018: update stock

--- wisecon/stock/__init__.py
+++ wisecon/stock/__init__.py
@@ -1,6 +1,6 @@
-from .forms import *
-from .index import *
-from .single_stock import *
-# need review
-from .sina import *
-from .trading_view import *
+# from .forms import *
+# from .index import *
+# from .single_stock import *
+# # need review
+# from .sina import *
+# from .trading_view import *
