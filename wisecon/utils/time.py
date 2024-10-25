2beadf19e7e158e00a8d67ed018758922ec0da6c
CaoChensy <867529463@qq.com>
2024-10-25 09:14:07 +0800

    20241024: add time

--- wisecon/utils/time.py
+++ wisecon/utils/time.py
@@ -10,7 +10,11 @@ __all__ = [
 
 
 def time2int() -> str:
-    """"""
+    """将当前时间转换为毫秒级时间戳
+
+    Returns:
+        毫秒级时间戳
+    """
     return str(int(time.time() * 1E3))
 
 
@@ -18,7 +22,16 @@ def year2date(
         year: int,
         format: Literal["%Y%m%d", "%Y-%m-%d"] = "%Y%m%d"
 ) -> Tuple[str, str]:
-    """"""
+    """给定一个年份，返回该年份的开始与结束日期
+
+    Args:
+        year: 年份
+        format: 日期格式
+
+    Returns:
+        start_date: 开始日期
+        end_date: 结束日期
+    """
     start_date = datetime(year, 1, 1).strftime(format)
     end_date = datetime(year, 12, 31).strftime(format)
     return start_date, end_date
