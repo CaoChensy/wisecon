67fd4d389c8818b2149b7c56b5f004ea14285b3c
CaoChensy <867529463@qq.com>
2025-05-08 10:59:58 +0800

    20250508: update mcp

--- wisecon/utils/scrapy_pdf.py
+++ wisecon/utils/scrapy_pdf.py
@@ -1,6 +1,7 @@
+import multiprocessing
+from typing import Dict
 from scrapy import Spider, Request
 from scrapy.crawler import CrawlerProcess
-from typing import Dict
 
 
 __all__ = [
@@ -35,10 +36,23 @@ class PDFSpider(Spider):
             self.result_obj.bytes_data = response.body
 
 
+def pdf_spider_worker(url, headers, return_dict):
+    """子进程执行函数"""
+    result_obj = PDFResponse()
+    process = CrawlerProcess({
+        'LOG_LEVEL': 'WARNING',
+        'LOG_FORMATTER': 'scrapy.logformatter.LogFormatter',
+    })
+    process.crawl(PDFSpider, target_url=url, result_obj=result_obj, headers=headers)
+    process.start()
+    return_dict['bytes'] = result_obj.bytes_data
+
+
 def fetch_pdf_bytes(url: str, headers: Dict) -> bytes:
     """"""
-    pdf_response = PDFResponse()
-    process = CrawlerProcess({'LOG_LEVEL': 'WARNING'})
-    process.crawl(PDFSpider, target_url=url, result_obj=pdf_response, headers=headers)
-    process.start()
-    return pdf_response.bytes_data
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    p = multiprocessing.Process(target=pdf_spider_worker, args=(url, headers, return_dict))
+    p.start()
+    p.join()
+    return return_dict.get('bytes')
