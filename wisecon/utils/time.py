21cd7d8f418d38eb26a522b2deeef5cdb48630fb
CaoChensy <867529463@qq.com>
2025-05-12 09:12:48 +0800

    20250511: update time

--- wisecon/utils/time.py
+++ wisecon/utils/time.py
@@ -6,6 +6,7 @@ from typing import Tuple, Literal
 __all__ = [
     "time2int",
     "year2date",
+    "is_quarter_end",
 ]
 
 
@@ -35,3 +36,38 @@ def year2date(
     start_date = datetime(year, 1, 1).strftime(format)
     end_date = datetime(year, 12, 31).strftime(format)
     return start_date, end_date
+
+
+def is_quarter_end(date: datetime | str | None = None) -> bool:
+    """判断给定日期是否为季度的最后一天
+
+    Args:
+        date_input: 判断是否为季度的最后一天，支持以下输入格式：
+            - None: 默认使用当前日期
+            - datetime 对象
+            - str 格式："yyyyMMdd" 或 "yyyy-MM-dd"
+
+    Returns: bool
+    """
+    if date is None:
+        date = datetime.today()
+    elif isinstance(date, datetime):
+        date = date
+    elif isinstance(date, str):
+        try:
+            if '-' in date:
+                date = datetime.strptime(date, "%Y-%m-%d")
+            else:
+                date = datetime.strptime(date, "%Y%m%d")
+        except ValueError:
+            raise ValueError("字符串格式错误，请使用 'yyyy-MM-dd' 或 'yyyyMMdd'")
+    else:
+        raise TypeError("不支持的数据类型，请传入 datetime 或 str")
+
+    quarter_ends = {
+        3: 31,
+        6: 30,
+        9: 30,
+        12: 31,
+    }
+    return date.month in quarter_ends and date.day == quarter_ends[date.month]
