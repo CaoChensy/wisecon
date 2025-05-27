ec2371f940b5c5b64e8e599891f98083e0b4157b
CaoChensy <867529463@qq.com>
2025-05-27 17:51:44 +0800

    update utils

--- test/utils/test_time.py
+++ test/utils/test_time.py
@@ -7,3 +7,16 @@ class TestTime(unittest.TestCase):
         """"""
         start_date, end_date = year2date(2022, format="%Y-%m-%d")
         print(start_date, type(start_date), end_date, type(end_date))
+
+    def test_get_quarter_ends(self):
+        """"""
+        result = get_quarter_ends('2023-03-31', '2024-03-31')
+        print(result)
+
+    def test_get_now_date(self):
+        """"""
+        result = get_now_date()
+        print(result)
+
+        result = get_now_date(format="%Y-%m-%d")
+        print(result)
